d68656cd510bea7f3950a2fea3af5eb669942903
Sarah Choi <chois727@newschool.edu>
2015-03-18 18:42:00 -0400

    commit

---
+++ week 6/testscript.js
@@ -0,0 +1,10 @@
+var name = prompt("what is your name?");
+
+document.getElementById("xholder").innerHTML = name;
+
+var color = prompt("what is your favorite color?");
+
+document.getElementById("xholder2").innerHTML = color;
+
+document.getElementById("xholder2").style.color = color;
+xholder.style.color = color;
