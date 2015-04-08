5e8a74d55d6d568499d83b8cf7c906f91695dbf7
Sarah Choi <chois727@newschool.edu>
2015-04-08 18:32:22 -0400

    commit

---
+++ week_7_hw_Madlib/madlib_script.js
@@ -0,0 +1,84 @@
+window.onload = function() {
+    var input1 = document.getElementById("WORD1");
+
+        input1.onclick = function() {
+            var result1 = prompt("Input word");
+            document.getElementById("word1").innerHTML = result1;
+        }
+        
+    var input2 = document.getElementById("WORD2");
+
+        input2.onclick = function() {
+            var result2 = prompt("Input word");
+            document.getElementById("word2").innerHTML = result2;
+        }
+        
+    var input3 = document.getElementById("WORD3");
+
+        input3.onclick = function() {
+            var result3 = prompt("Input word");
+            document.getElementById("word3").innerHTML = result3;
+        }
+        
+    var input4 = document.getElementById("WORD4");
+
+        input4.onclick = function() {
+            var result4 = prompt("Input word");
+            document.getElementById("word4").innerHTML = result4;
+        }
+        
+    var input5 = document.getElementById("WORD5");
+
+        input5.onclick = function() {
+            var result5 = prompt("Input word");
+            document.getElementById("word5").innerHTML = result5;
+        }
+
+    var input6 = document.getElementById("WORD6");
+
+        input6.onclick = function() {
+            var result6 = prompt("Input word");
+            document.getElementById("word6").innerHTML = result6;
+        }
+
+    var input7 = document.getElementById("WORD7");
+
+        input7.onclick = function() {
+            var result7 = prompt("Input word");
+            document.getElementById("word7").innerHTML = result7;
+        }
+        
+    var input8 = document.getElementById("WORD8");
+
+        input8.onclick = function() {
+            var result8 = prompt("Input word");
+            document.getElementById("word8").innerHTML = result8;
+        }
+        
+    var input9 = document.getElementById("WORD9");
+
+        input9.onclick = function() {
+            var result9 = prompt("Input word");
+            document.getElementById("word9").innerHTML = result9;
+        }
+        
+    var input10 = document.getElementById("WORD10");
+
+        input10.onclick = function() {
+            var result10 = prompt("Input word");
+            document.getElementById("word10").innerHTML = result10;
+        }
+
+    var input11 = document.getElementById("WORD11");
+
+        input11.onclick = function() {
+            var result11 = prompt("Input word");
+            document.getElementById("word11").innerHTML = result11;
+        }        
+}
+
+
+    function SeeResult() {
+            var myPara = document.getElementById("changeText");
+            myPara.style.color = "black";
+            }    
