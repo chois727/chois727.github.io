3356b77a1f928b9595e9b6a7c08f1cc48a0e48f3
Sarah Choi <chois727@newschool.edu>
2015-05-06 00:56:28 -0400

    commit

--- FINAL/js/index.js
+++ FINAL/js/index.js
@@ -1,20 +1,40 @@
  $(document).ready(function () {
             $('.flag').waypoint(function (direction) {
                 if (direction == 'down') {
-                    $('header').css("height", "5vh");
-                    $('header').css("font-size", "100%");
+                    $('header').css("height", "2vh");
+                    $('.logo').css("width", "6vh");
+                    $('.logo').css("height", "2vh"); 
+                    $('.logo').css("background", "#078374");
+                    $('.logo').css("margin", "1%");
+                    $('subscribe').css("opacity", "1");
+                    $('.nav-wo').css("font-size", "0%");   
+                    $('.nav-wo').css("background", "orange");
+                    $('.nav-wo').css("width", "10px");
+                    $('.nav-wo').css("height", "10px");
+                    $('.nav-wo').css("margin-left", "21%");
+                    $('.nav').css("margin-top", "1%");
                     $('section').css("height", "95vh");
                     $('container').css("top", "5vh");
                 }
                 if (direction == 'up') {
                     $('header').css("height", "15vh");
+                    $('.logo').css("height", "10vh");
+                    $('.logo').css("width", "10vh");
+                    $('.logo').css("margin-top", "0%");
+                    $('.logo').removeAttr("style");
+                    $('.nav-wo').removeAttr("style");       
+                    $('.nav').removeAttr("style");
                     $('header').css("font-size", "200%");
                     $('section').css("height", "85vh");
                     $('container').css("top", "15vh");
+
                 }
             }, {
                 offset: '99%'
             });
+     
+
+
      
       function requestFile(url){
             $.ajax(
@@ -43,8 +63,20 @@
             requestFile("Section3.html");
             }
         ) 
+         
+          $("#ajaxrequestbanner1").click(function(){
+            requestFile("Section1.html");
+            }
+        ) 
 
-
+         $(window).scroll(function() {    
+        var scroll = $(window).scrollTop();
+        $('.FPscroll').stop().animate(
+        {opacity: (( 180-scroll )/100)+0.1},
+        "slow"
+    );
+});
+         
 });
 
 
