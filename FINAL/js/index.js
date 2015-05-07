8d6cb73b9328ae8ca0383e806808b56791ce05fd
Sarah Choi <chois727@newschool.edu>
2015-05-07 01:25:40 -0400

    commit

--- FINAL/js/index.js
+++ FINAL/js/index.js
@@ -1,12 +1,13 @@
  $(document).ready(function () {
             $('.flag').waypoint(function (direction) {
                 if (direction == 'down') {
-                    $('header').css("height", "2vh");
+                    $('header').css("height", "10px");
                     $('.logo').css("width", "6vh");
                     $('.logo').css("height", "2vh"); 
                     $('.logo').css("background", "#078374");
                     $('.logo').css("margin", "1%");
                     $('subscribe').css("opacity", "1");
+                    $('.hidebutton').css("opacity", "1");
                     $('.nav-wo').css("font-size", "0%");   
                     $('.nav-wo').css("background", "orange");
                     $('.nav-wo').css("width", "10px");
@@ -17,16 +18,18 @@
                     $('container').css("top", "5vh");
                 }
                 if (direction == 'up') {
-                    $('header').css("height", "15vh");
+                    $('header').removeAttr("style");
                     $('.logo').css("height", "10vh");
                     $('.logo').css("width", "10vh");
                     $('.logo').css("margin-top", "0%");
                     $('.logo').removeAttr("style");
                     $('.nav-wo').removeAttr("style");       
                     $('.nav').removeAttr("style");
+                    $('.hidebutton').css("opacity", "0");       
                     $('header').css("font-size", "200%");
                     $('section').css("height", "85vh");
                     $('container').css("top", "15vh");
+                    
 
                 }
             }, {
@@ -43,6 +46,8 @@
                     cache: false,
                     success: function(data){
                         $("#ajaxcontainer").html(data);
+                        $("html, body").animate({ scrollTop: 0 }, 200);
+
                     }
                 }
             );
@@ -69,15 +74,48 @@
             }
         ) 
 
+          
+          
+/*----scroll disappear----*/          
          $(window).scroll(function() {    
         var scroll = $(window).scrollTop();
         $('.FPscroll').stop().animate(
         {opacity: (( 180-scroll )/100)+0.1},
         "slow"
-    );
+        );
+        })
+/*----scroll disappear end----*/ 
+         
+       
+        $(function(){
+            $('hidebutton').click(function(){
+                $('header.hidden').toggleClass('.shrunk');
+            
+            })
+        
+        })
+        
+        
+                $('.pop-up').hide();
+
+        
+            $(document).delegate('.subscribe', 'click', function(){
+                $('.pop-up').fadeIn(500);
+                $('#overlay').removeClass('blur-out');
+                $('#overlay').addClass('blur-in');
+
+                $('.close-button').click(function (e) { 
+                    $('.pop-up').fadeOut(200);
+                    $('#overlay').removeClass('blur-in');
+                    $('#overlay').addClass('blur-out');
+                    e.stopPropagation();
+                });
+            });
+            
+
 });
          
-});
+
 
 
 
